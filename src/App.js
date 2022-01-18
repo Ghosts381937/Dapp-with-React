8882a4eee70f2c0f83af36b7b6a9b5d91dc425d5
Ghosts381937 <she.xing88@gmail.com>
2022-01-18 15:44:01 +0800

    Done for expample

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import Web3 from 'web3';
+import {CONTACT_ADDRESS, CONTACT_ABI} from './config';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [account, setAccount] = useState(); // state variable to set account.
+  const [web3, setWeb3] = useState();
+  const [contract, setContract] = useState();
+  const [totalSupply, setTotalSupply] = useState();
+  const [chainId, setChainId] = useState();
+  const [GHOBalance, setGHOBalance] = useState();
+  const connectWallet = () => {
+    window.ethereum.request({ method: 'eth_requestAccounts' })
+    .then((_account) => {setAccount(_account[0]);console.log(_account);});
+  }
+  const handleTranfer = () => {
+    contract.methods.transfer({_to}, web3.utils.toWei('1', 'ether'))
+    .send({from: account});
+  }
+  const handleApprove = () => {
+    contract.methods.approve(account, web3.utils.toWei('1', 'ether'))
+    .send({from: account});
+  }
+  useEffect(() => {
+    async function init() {
+      await window.ethereum.request({
+        method: 'wallet_switchEthereumChain',
+        params: [{ chainId: '0x61' }],
+      });
+      const _web3 = new Web3(window.ethereum);
+      const _account = await _web3.eth.getAccounts();
+      const _chainId = await _web3.eth.net.getId();
+      const _contract = new _web3.eth.Contract(CONTACT_ABI, CONTACT_ADDRESS);
+      const _totalSupply = await _contract.methods.totalSupply().call();
+      const _balance = await _contract.methods.balanceOf(_account[0]).call();
+      window.ethereum.on('chainChanged',(_chainId) => setChainId(_chainId));
+      window.ethereum.on('accountsChanged', (_account) => setAccount(_account[0]));
+      setWeb3(_web3);
+      setAccount(_account[0]);
+      setChainId(_chainId);
+      setContract(_contract);
+      setTotalSupply(_web3.utils.fromWei(_totalSupply, 'ether'));
+      setGHOBalance(_web3.utils.fromWei(_balance, 'ether'));
+    }
+    init();
+   }, [web3]);
+    return (
+      <div>
+        Your account is: {account}
+        <br />
+        TotalSupply is: {totalSupply}
+        <br />
+        ChainId is: {chainId}
+        <br />
+        GHO is: {GHOBalance}
+        <br />
+        <button onClick={connectWallet}>Connect Wallet</button>
+        <br />
+        <button onClick={handleTranfer}>Transfer</button>
+        <br />
+        <button onClick={handleApprove}>Approve</button>
+      </div>
+    );
 }
 
 export default App;
